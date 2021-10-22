a67a2c6741ea736827584064543698fe339c6959
Moses Holmstrom <thykka@gmail.com>
2021-10-22 23:20:14 +0300

    read pixel data

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const startTime = process.hrtime();
 // https://www.npmjs.com/package/node-webcam
 const nodeWebcam = require('node-webcam');
-
+// https://github.com/dy/image-pixels
+const pixels = require('image-pixels');
 
 const options = {
-	width: 1920/8,
-	height: 1080/8,
+	sourceWidth: 640,
+	sourceHeight: 480,
+	outWidth: 120,
+	outHeight: 32
+};
+
+const webcamOptions = {
+	width: options.sourceWidth,
+	height: options.sourceHeight,
 	quality: 100,
 	frames: 1,
 	delay: 0,
@@ -14,15 +21,37 @@ const options = {
 	//device: false,
 	callbackReturn: 'base64'
 };
+const pixOptions = {
+	type: 'image/png',
+	//clip: [0,0,640,480],
+	clip: [
+		options.sourceWidth/2-options.outWidth/2,
+		options.sourceHeight/2-options.outHeight/2,
+		options.sourceWidth/2+options.outWidth/2,
+		options.sourceHeight/2+options.outHeight/2
+	].map(v => Math.round(v))
+};
 
-const cam = nodeWebcam.create(options);
-const capTime = process.hrtime();
-cam.capture('test', (err, data) => {
-	const endTime = process.hrtime();
-	if(err) {
-		throw(err);
+const cam = nodeWebcam.create(webcamOptions);
+
+const glyphs = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`\'. '.split('').reverse();
+
+async function processWebcamData(error, data) {
+	if(error) {
+		throw(error);
 	}
-	const buf = Buffer.from(data, 'base64');
-	console.log(buf.length)
-	console.log(capTime, endTime, endTime[0] - capTime[0])
-})
+	const imageData = await pixels(data,pixOptions);
+	//console.log(imageData);
+	const g = [];
+	for(let i = 0,v; i<imageData.data.length; i+=4) {
+		v=imageData.data[i+1]||0;
+		g.push(glyphs[Math.floor(v/255*glyphs.length)])
+	}
+	console.log(g.join(''))
+	getEntropy();
+}
+function getEntropy() {
+	cam.capture('test', processWebcamData);
+}
+
+getEntropy();
