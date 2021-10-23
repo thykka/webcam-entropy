bb683dae11aacbd8651857166f69c6e80bf8e9a3
Moses Holmstrom <thykka@gmail.com>
2021-10-23 15:45:54 +0300

    output random

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const pixels = require('image-pixels');
 const options = {
 	sourceWidth: 640,
 	sourceHeight: 480,
-	outWidth: 120,
-	outHeight: 32
+	outWidth: 640,
+	outHeight: 480
 };
 
 const webcamOptions = {
@@ -40,16 +40,45 @@ async function processWebcamData(error, data) {
 	if(error) {
 		throw(error);
 	}
-	const imageData = await pixels(data,pixOptions);
-	//console.log(imageData);
+	let imageData = await pixels(data,pixOptions);
+	if(imageData.data[0]!==0) { // ensure this isn't a blank frame
+		whiten(imageData.data)
+	}
+	delete imageData;
+	setTimeout(()=>getEntropy(),1)
+}
+
+const crypto = require('crypto');
+const fs = require('fs');
+fs.unlinkSync('test.dat');
+const stream = fs.createWriteStream('test.dat',{flags:'a'})
+const gen = require('random-seed');
+
+let hash, rndGen;
+
+function whiten(data) {
+	hash = crypto.createHash('blake2b512').update(data).digest('utf8');
+	rndGen = gen.create(hash);
+	for(let i=0;i<1024;i++){
+		stream.write(String.fromCharCode(rndGen(Number.MAX_SAFE_INTEGER)));
+	}
+}
+
+process.on('SIGINT', () => {
+	stream.end();
+	console.log('Wrote file.')
+	process.exit();
+})
+
+function displayAsciiImage(imageData) {
 	const g = [];
 	for(let i = 0,v; i<imageData.data.length; i+=4) {
 		v=imageData.data[i+1]||0;
 		g.push(glyphs[Math.floor(v/255*glyphs.length)])
 	}
 	console.log(g.join(''))
-	getEntropy();
 }
+
 function getEntropy() {
 	cam.capture('test', processWebcamData);
 }
